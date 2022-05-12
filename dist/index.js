3d1d2430608c2653d7177af3cf89701b3e644b19
Cassiano Aquino <caquino@users.noreply.github.com>
2022-05-12 11:45:30 +0100

    suggestions

--- dist/index.js
+++ dist/index.js
@@ -33,53 +33,71 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // standard actions and github api libraries
 const core = __importStar(require("@actions/core"));
+const process_1 = require("process");
 const YAML = __importStar(require("yamljs"));
 const checks_1 = __importDefault(require("./checks"));
-const path_1 = __importDefault(require("path"));
+const path = __importStar(require("path"));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        core.info("Starting health check");
-        const wd = process.env[`GITHUB_WORKSPACE`] || "";
-        const yamlFile = core.getInput("docker-compose-file") || path_1.default.join(wd, "docker-compose.yml");
-        const composeConfig = YAML.load(yamlFile);
-        // grab the services from the compose file
-        const services = composeConfig.services;
-        // loop through the services
-        for (const serviceName in services) {
-            // get the service definition
-            const service = services[serviceName];
-            // loop trought the ports on the service definition
-            for (const port of service.ports) {
-                let portNumber = 0;
-                // convert the port to a number
-                switch (typeof port) {
-                    case "string": {
-                        portNumber = Number(port.split(":")[0]);
-                        if (isNaN(portNumber)) {
-                            core.setFailed(`Service: ${serviceName} / Value: ${port} / Invalid format`);
+        try {
+            core.info("Starting health check 🚀");
+            const wd = process.env[`GITHUB_WORKSPACE`] || "";
+            core.debug(`GITHUB_WORKSPACE: ${wd}`);
+            const yamlFile = core.getInput("docker-compose-file") || path.join(wd, "docker-compose.yml");
+            core.debug(`Using docker-compose file: ${yamlFile}`);
+            const composeConfig = YAML.load(yamlFile);
+            core.debug(`Loaded docker-compose file: ${JSON.stringify(composeConfig)}`);
+            // grab the services from the compose file
+            const services = composeConfig.services;
+            core.debug(`Found ${Object.keys(services).length} services`);
+            // loop through the services
+            for (const serviceName in services) {
+                core.debug(`Checking service: ${serviceName}`);
+                // get the service definition
+                const service = services[serviceName];
+                core.debug(`Found service: ${JSON.stringify(service)}`);
+                core.debug(`Found ${Object.keys(service.ports).length} ports`);
+                // loop trought the ports on the service definition
+                for (const port of service.ports) {
+                    core.debug(`Checking port: ${port}`);
+                    let portNumber = 0;
+                    // convert the port to a number
+                    switch (typeof port) {
+                        case "string": {
+                            portNumber = Number(port.split(":")[0]);
+                            if (isNaN(portNumber)) {
+                                core.setFailed(`Service: ${serviceName} / Value: ${port} / Invalid format`);
+                            }
+                            break;
                         }
-                        break;
+                        case "number": {
+                            portNumber = Number(port);
+                            break;
+                        }
+                        default: {
+                            core.setFailed(`Service: ${serviceName} / Value: ${port} / Invalid port type: ${typeof port}`);
+                            process_1.exit(1);
+                            break;
+                        }
                     }
-                    case "number": {
-                        portNumber = Number(port);
-                        break;
+                    core.info(`Checking port ${portNumber} for service ${serviceName}...`);
+                    if (yield checks_1.default(portNumber)) {
+                        core.info(`Service: ${serviceName} / Port: ${portNumber} / Status: 🟢`);
                     }
-                    default: {
-                        core.setFailed(`Service: ${serviceName} / Value: ${port} / Invalid port type: ${typeof port}`);
-                        // log error
-                        break;
+                    else {
+                        core.setFailed(`Service: ${serviceName} / Port: ${portNumber} / Status: 🔴`);
                     }
-                }
-                core.info(`Checking port ${portNumber} for service ${serviceName}...`);
-                if (yield checks_1.default(portNumber)) {
-                    core.info(`Service: ${serviceName} / Port: ${portNumber} / Status: up`);
-                }
-                else {
-                    core.setFailed(`Service: ${serviceName} / Port: ${portNumber} / Status: down`);
                 }
             }
         }
-        core.info("Finished health check");
+        catch (error) {
+            if (error instanceof Error) {
+                core.setFailed(error.message);
+            }
+        }
+        finally {
+            core.info("Finished health check...");
+        }
     });
 }
 void run();
